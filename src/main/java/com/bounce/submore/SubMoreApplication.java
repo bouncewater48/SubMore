6b73d844f67ae89e535f5ac976e140e2dc6fffba
bouncewater <bouncewater48@gmail.com>
2026-01-13 17:52:38 +0900

    [강승훈] - 초기 설정 세팅.

---
+++ src/main/java/com/bounce/submore/SubMoreApplication.java
@@ -0,0 +1,13 @@
+package com.bounce.submore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubMoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubMoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bounce/submore/SubMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bounce.submore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubMoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
